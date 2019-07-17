1022d0c271a0b9dec2c8edfa12877ba97a33304f
Jarod Christiansen <JarodChristiansen2@gmail.com>
2019-07-17 10:48:04 -0700

    added buttons and implemented javascript to display on click, building another UI for youtube, soundcloud and study paper API's

---
+++ Recommended Content/main.js
@@ -0,0 +1,10 @@
+document.getElementById("physical").addEventListener('click', function(){
+    document.getElementById("morp").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'block';
+    document.getElementById("myDIV").style = 'margin-left: 22vw';
+})
+document.getElementById("mental").addEventListener('click', function(){
+    document.getElementById("morp").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'block';
+    document.getElementById("myDIV").style = 'margin-left: 22vw';
+})
