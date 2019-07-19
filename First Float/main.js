83678d33fafa332fb74e1da99b93e042128d7d57
Jarod Christiansen <JarodChristiansen2@gmail.com>
2019-07-18 17:41:17 -0700

    add services, format First float

--- First Float/main.js
+++ First Float/main.js
@@ -1,1 +1,11 @@
         onmouseover="document.getElementById('div1').style.display = 'block';"
+
+document.getElementById('physicalBenefits').addEventListener('click', function(){
+ document.getElementById('muscleRecovery').style.display = 'block';
+ document.getElementById('muscleRecovery').style.color = "black";       
+});
+
+
+document.getElementById('muscleRecovery').addEventListener('click', function(){
+        document.getElementById('muscleRecovery').style.display = 'none';
+})
